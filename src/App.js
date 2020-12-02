eb8fed61159c4e13b6e0d71c8ca23b7989daf567
ross-u <cirkovic.uros.srb@gmail.com>
2020-12-02 18:40:21 +0100

    Update file structure and imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import DashboardWRedirect from './components/DashboardWRedirect';
-import Dashboard from './components/Dashboard';
-import About from './components/About';
-import Contact from './components/Contact';
-import Error from './components/Error';
-import Navbar from './components/Navbar';
-import { Projects } from './components/Projects';
+// import DashboardWRedirect from './pages/DashboardWRedirect';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
+import Navbar from './pages/Navbar';
+import { Projects } from './pages/Projects';
 
 import './App.css';
 
